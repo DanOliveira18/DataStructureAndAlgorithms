92cf66221be6fef3dcb442a85a2ed8644fbdfc60
Daniel Oliveira <daniel.santos@venturus.org.br>
2024-11-26 23:22:33 -0300

    leetcode 112

---
+++ src/leetcode/112.ts
@@ -0,0 +1,27 @@
+//Given the root of a binary tree and an integer targetSum,
+//return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+//A leaf is a node with no children.
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  if (!root) return false;
+
+  if (!root.left && !root.right && targetSum === root.val) {
+    return true;
+  }
+
+  return (
+    hasPathSum(root.left, targetSum - root.val) ||
+    hasPathSum(root.right, targetSum - root.val)
+  );
+}
